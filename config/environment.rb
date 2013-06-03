f6fdcedbbbbcd193a714a68b7e171141dbe87cfa
Ingo Kampe <ingo.kampe@kreuzwerker.de>
2013-06-03 23:14:38 +0200

    migrated to bundler

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ ENV['RAILS_ENV'] ||= 'development'
 #ENV['GEM_HOME'] ||= '/opt/gems'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+#RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -26,9 +26,9 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
-  config.gem 'mini_magick'
-  config.gem 'geokit'
+  #config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
+  #config.gem 'mini_magick'
+  #config.gem 'geokit'
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
